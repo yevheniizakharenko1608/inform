d24923a24c46a2447bab794a8a40e5cf42731d32
yevheniizakharenko1608 <yevheniizakharenko@yandex.ru>
2016-07-31 02:25:28 +0300

    #added slider

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Slider from '../Slider'
 
 export default class Home extends Component {
+    constructor(){
+        super();
+        this.state = {
+            meaning: 0
+        };
+        this.update = this.update.bind(this)
+    }
+
+    update(){
+        this.setState({
+            meaning: ReactDOM.findDOMNode(this.refs.meaning.refs.inp).value
+        })
+    }
+
     render() {
         return (
             <div className='row'>
-                <div className='col-md-12'>Добро пожаловать на курс по redux + react-routerz</div>
+                <div className='col-lg-1 col-md-1 col-sm-1 col-xs-1'></div>
+                <Slider className='col-lg-10 col-md-10 col-sm-10 col-xs-10'
+                        ref='meaning'
+                min={0}
+                max={100} 
+                step={1}
+                val={this.state.meaning}
+                label='How old are you?'
+                update={this.update}/>
+                <div className='col-lg-1 col-md-1 col-sm-1 col-xs-1'></div>
             </div>
         )
     }
